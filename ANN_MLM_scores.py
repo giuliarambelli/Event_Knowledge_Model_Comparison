73a547892807c4ac7aab2adaf9caba25dfb47c29
carina-kauf <ckauf@mit.edu>
2022-03-03 11:09:19 -0500

    annotate code

--- ANN_MLM_scores.py
+++ ANN_MLM_scores.py
@@ -11,7 +11,6 @@ import tqdm
 # -*- coding: utf-8 -*-
 
 _logger = logging.getLogger(__name__)
-debug = False
 
 class TransformerModel:
 
@@ -22,28 +21,31 @@ class TransformerModel:
 
     def prepare_input(self, sentence):
         tokens = self.tokenizer.tokenize(sentence)
-        print(tokens)
+        _logger.debug(f"{tokens}")
         nr_tokens = len(tokens)
         list_of_sents = [sentence] * nr_tokens
         return tokens, nr_tokens, list_of_sents
 
     def compute_filler_probabilities(self, tokens, nr_tokens, list_of_sents, l2r=False):
-        if debug: print(list_of_sents)
+        """
+        :param tokens: sentence tokens
+        :param nr_tokens: nr. of sentence tokens
+        :param list_of_sents: list containing nr_tokens-times input sentence > used for masking word-by-word
+        :param l2r: whether to only use leftward context or not
+        :return: list containing pseudo-log-likelihoods per token
+        """
         inputs = self.tokenizer(list_of_sents, padding=True, return_tensors="pt")
         for i in range(nr_tokens):
             inputs['input_ids'][i][i + 1] = self.tokenizer.mask_token_id  # i+1 due to ['CLS'] token, whose index is 101
-            if debug: print(inputs['input_ids'][i])
-        if debug: print(inputs)
+            _logger.debug(f"{inputs['input_ids'][i]}")
+        _logger.debug(f"{inputs}")
 
         log_probs_fillers = []
 
         if not l2r:
-            outputs = self.mlm_model(**inputs)[
-                0]  # get loss, with **inputs pass entire input, with attention mask to model
-            if debug: print(outputs.shape)
+            outputs = self.mlm_model(**inputs)[0]  # get loss | **inputs passes entire input dict to model
 
             for batch_elem, token, index in zip(range(outputs.shape[0]), tokens, range(1, nr_tokens + 1)):
-                if debug: print(index, token)
                 all_log_probs = torch.nn.functional.log_softmax(outputs[batch_elem, index])
                 log_probs_fillers.append(all_log_probs[self.tokenizer.convert_tokens_to_ids(token)].item())
                 _logger.debug(
@@ -51,15 +53,12 @@ class TransformerModel:
 
         else:  # if l2r
             for i in range(nr_tokens):
-                if debug: print(inputs['attention_mask'][i])
                 inputs['attention_mask'][i][i + 1:] = 0  # i+1 due to ['CLS'] token, whose index is 101
-                if debug: print(inputs['attention_mask'][i])
+                _logger.debug(f"{inputs['attention_mask'][i]}")
 
             outputs = self.mlm_model(**inputs)[0]
-            if debug: print(outputs.shape)
 
             for batch_elem, token, index in zip(range(outputs.shape[0]), tokens, range(1, nr_tokens + 1)):
-                if debug: print(index, token)
                 all_log_probs = torch.nn.functional.log_softmax(outputs[batch_elem, index])  # log_softmax
                 log_probs_fillers.append(all_log_probs[self.tokenizer.convert_tokens_to_ids(token)].item())
                 _logger.debug(
@@ -69,21 +68,39 @@ class TransformerModel:
 
 
 def get_sentence_score(model, sentences, average=False, l2r=False):
+    """
+    Run function to get sequence scores
+    :param model: Transformer model
+    :param sentences: list of sentences for which scores are to be computed
+    :param average: Whether to length-normalize scores or not
+    :param l2r: whether to only use leftward context or not
+    :return: list of PLL/Unidirectional-PLL scores for sequences
+    """
     sentence_scores = []
     for sent in tqdm.tqdm(sentences):
         tokens, nr_tokens, list_of_sents = model.prepare_input(sent)
         probabilities_fillers = model.compute_filler_probabilities(tokens, nr_tokens, list_of_sents, l2r=l2r)
-        sentence_score = sum(probabilities_fillers)  # returns log likelihood score
+        sentence_score = sum(probabilities_fillers)  # returns pseudo log likelihood score
         if average:
             nr_words = len(sent.split())
             sentence_score = sentence_score / nr_words
         sentence_scores.append(sentence_score)
-        print("*****")
+        _logger.info(f" {tokens} | {sentence_score}")
 
     return sentence_scores
 
 
 def get_word_score(model, model_name, sentences, verb_indices=None, average=False, l2r=False):
+    """
+    Run function to get word scores
+    :param model: Transformer model
+    :param model_name: model version
+    :param sentences: list of sentences for which scores are to be computed
+    :param verb_indices: list of verb indices
+    :param average: Whether to length-normalize scores or not
+    :param l2r: whether to only use leftward context or not
+    :return: list of log probability scores for target word per sequence (either verb or last word)
+    """
     word_scores = []
 
     if verb_indices is None:  # if last word probability
@@ -92,13 +109,11 @@ def get_word_score(model, model_name, sentences, verb_indices=None, average=Fals
         word_indices = verb_indices
 
     for ind, sent in zip(word_indices, sentences):
-        if debug: print(sent)
 
         word = sent.split()[ind]
 
         if word.endswith("."):  # exclude final period
             word = word.rstrip(".")
-        print(f"Getting probability for word: {word}")
 
         if model_name.startswith("roberta"):
             word_tokens = model.tokenizer.tokenize(f" {word}")  # account for Byte-level BPE tokenizer special behavior
@@ -106,35 +121,38 @@ def get_word_score(model, model_name, sentences, verb_indices=None, average=Fals
             word_tokens = model.tokenizer.tokenize(word)
 
         sent_tokens = model.tokenizer.tokenize(sent)
-        if debug: print(word_tokens, sent_tokens)
+        _logger.debug(f"{word_tokens}, {sent_tokens}")
 
         word_starts = [i for i, x in enumerate(sent_tokens) if x == word_tokens[0]]
         if len(word_starts) == 1:
             word_start = word_starts[0]
             word_span = len(word_tokens)
-            if debug: print(word_start, word_span)
+            _logger.debug(f"{word_start}, {word_span}")
             assert all(sent_tokens[word_start + i] == word_tokens[i] for i in range(word_span))
         else:
             for ind, word_start in enumerate(word_starts):
                 word_start = word_starts[ind]
                 word_span = len(word_tokens)
-                if debug: print(word_start, word_span)
+                _logger.debug(f"{word_start}, {word_span}")
                 if all(sent_tokens[word_start + i] == word_tokens[i] for i in range(word_span)):
                     break
 
         tokens, nr_tokens, list_of_sents = model.prepare_input(sent)
         probabilities_fillers = model.compute_filler_probabilities(tokens, nr_tokens, list_of_sents, l2r=l2r)
-        print(probabilities_fillers[word_start:word_start + word_span])
+        _logger.info(f" {word} | {probabilities_fillers[word_start:word_start + word_span]}")
         word_score = sum(probabilities_fillers[word_start:word_start + word_span])  # returns log likelihood score
         if average:
             word_score = word_score / word_span
         word_scores.append(word_score)
-        print("*****")
 
     return word_scores
 
 
 def prepare_data(df):
+    """
+    :param df: dataset containing sentences and the word position id of the word per sentence
+    :return: list of verb position ids & list of preprocessed sentences
+    """
     verb_ids = []
     sents = []
     for index, row in df.iterrows():
@@ -146,6 +164,19 @@ def prepare_data(df):
     return verb_ids, sents
 
 def main():
+    """
+    Main run function for one of 4 input score types:
+    * w2w: This implements pseudo-log-likelihood scores of a sequence as proposed by Salazar et al. (2019)
+            Results obtained are equivalent as from https://github.com/awslabs/mlm-scoring
+    * l2r: This implements pseudo-log-likelihood scores with access only to the unidirectional context
+            (Implemented via attention masks, "most like" GPT2 scoring, but unlike MLM's training objective)
+    * verb: Getting the log probability of a verb, relies on verb_ids from df
+    * last_word: Getting the log probability of the last word in the sequence
+
+    * average flag can be set to get
+        - word-length normalized PLL scores for sequences (see Salazar et al. (2019), Appendix C)
+        - word-scores normalized by subtoken number
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('--log_level', type=str, default='INFO')
     parser.add_argument('--which_score', nargs='+', default=['w2w', 'l2r', 'verb', 'last_word'])

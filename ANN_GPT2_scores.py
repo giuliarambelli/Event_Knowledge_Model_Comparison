fce478f572ff060b6c07f6694560ae6ff8a9dd19
carina-kauf <ckauf@mit.edu>
2023-08-17 23:08:41 -0400

    scaling

--- ANN_GPT2_scores.py
+++ ANN_GPT2_scores.py
@@ -1,4 +1,4 @@
-from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPTNeoForCausalLM, AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, GPT2Tokenizer, BloomModel
 
 import torch
 import pandas as pd
@@ -9,7 +9,7 @@ import tqdm
 
 import os
 
-from ANN_MLM_scores import prepare_data
+from ANN_MLM_adapted import prepare_data
 
 # -*- coding: utf-8 -*-
 
@@ -31,19 +31,36 @@ def score(model, tokenizer, sentence):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--log_level', type=str, default='INFO')
-    parser.add_argument('--dataset_names', nargs='+', default=['ev1', 'dtfit', 'new-EventsAdapt'])
+    parser.add_argument('--dataset_names', nargs='+', default=['new-EventsAdapt','ev1', 'dtfit'])
     parser.add_argument('--versions', nargs='+', default=['gpt2-xl', 'gpt-j', 'gpt2-medium'])
     args = parser.parse_args()
 
-    dict_tokenizers = {"gpt2-medium": GPT2Tokenizer.from_pretrained('gpt2-medium'),
-                       "gpt2-xl": GPT2Tokenizer.from_pretrained('gpt2-xl'),
-                       "gpt-neo": GPT2Tokenizer.from_pretrained('EleutherAI/gpt-neo-1.3B'),
-                       "gpt-j": AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")}
+#     dict_tokenizers = {"distilgpt2": AutoTokenizer.from_pretrained('distilgpt2'),
+#                        "openai-gpt": AutoTokenizer.from_pretrained('openai-gpt'),
+#                        "gpt2": AutoTokenizer.from_pretrained('gpt2'),
+#                        "gpt2-medium": AutoTokenizer.from_pretrained('gpt2-medium'),
+#                        "gpt2-large": AutoTokenizer.from_pretrained('gpt2-large'),
+#                        "gpt2-xl": AutoTokenizer.from_pretrained('gpt2-xl'),
+#                        #"gpt-neo": GPT2Tokenizer.from_pretrained('EleutherAI/gpt-neo-1.3B'), #https://github.com/artidoro/qlora/issues/123 https://github.com/huggingface/transformers/issues/17756#issuecomment-1534219526
+#                        #"gpt-j": AutoTokenizer.from_pretrained('EleutherAI/gpt-j-6B'),
+#                        "mpt-7b": AutoTokenizer.from_pretrained('mosaicml/mpt-7b'),
+#                        "mpt-30b": AutoTokenizer.from_pretrained('mosaicml/mpt-30b'),
+#                        #"bloom-7b1" : AutoTokenizer.from_pretrained("bigscience/bloom-7b1")
+#                        # if we want bloom, check this: https://github.com/huggingface/transformers/issues/24514
+#                        }
 
-    dict_models = {"gpt2-medium": GPT2LMHeadModel.from_pretrained('gpt2-medium'),
-                   "gpt2-xl": GPT2LMHeadModel.from_pretrained('gpt2-xl'),
-                   "gpt-neo": GPTNeoForCausalLM.from_pretrained('EleutherAI/gpt-neo-1.3B'),
-                   "gpt-j": AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B")}
+#     dict_models = {"distilgpt2": AutoModelForCausalLM.from_pretrained('distilgpt2'),
+#                    "openai-gpt": AutoModelForCausalLM.from_pretrained('openai-gpt'),
+#                    "gpt2": AutoModelForCausalLM.from_pretrained('gpt2'),
+#                    "gpt2-medium": AutoModelForCausalLM.from_pretrained('gpt2-medium'),
+#                    "gpt2-large": AutoModelForCausalLM.from_pretrained('gpt2-large'),
+#                    "gpt2-xl": AutoModelForCausalLM.from_pretrained('gpt2-xl'),
+#                    #"gpt-neo": AutoModelForCausalLM.from_pretrained('EleutherAI/gpt-neo-1.3B'),
+#                    #"gpt-j": AutoModelForCausalLM.from_pretrained('EleutherAI/gpt-j-6B'),
+#                    "mpt-7b": AutoModelForCausalLM.from_pretrained('mosaicml/mpt-7b', trust_remote_code=True),
+#                    "mpt-30b": AutoModelForCausalLM.from_pretrained('mosaicml/mpt-30b', trust_remote_code=True)#,
+#                    #"bloom-7b1": BloomModel.from_pretrained("bigscience/bloom-7b1")
+#                    }
 
     out_dir = 'results/ANNs/'
     os.makedirs(out_dir, exist_ok=True)
@@ -59,21 +76,21 @@ def main():
 
     # Load pre-trained model (weights)
     for version in args.versions:
-        model = dict_models[version]
+        model = AutoModelForCausalLM.from_pretrained(version)
         model.eval()
         # Load pre-trained model tokenizer (vocabulary)
-        tokenizer = dict_tokenizers[version]
+        tokenizer = AutoTokenizer.from_pretrained(version)
         _logger.info(f"*********** Getting scores for model {version} ***********")
         for dataset_name in args.dataset_names:
             _logger.info(f"*********** Processing: {dataset_name} ***********")
             _, sentences = datasets[dataset_name]
-            
+
             sent_scores = []
             sent_scores_avg_by_nrtokens = []
             sent_scores_avg_by_nrwords = []
             nr_tokens = []
             nr_words = []
-            
+
             for sent in tqdm.tqdm(sentences):
                 sent_score, sent_score_avg_by_nrtoken, sent_score_avg_by_nrwords, nr_tok, nr_word = score(model, tokenizer, sent)
                 sent_scores.append(sent_score)
@@ -87,13 +104,13 @@ def main():
             with open(out_name, "w") as fout:
                 for i, sent, sent_score, nr_tok, nr_word in zip(range(len(sentences)), sentences, sent_scores, nr_tokens, nr_words):
                     fout.write(f'{i}\t{sent}\t{sent_score}\t{nr_tok}\t{nr_word}\n')
-                    
+
             out_name = os.path.join(out_dir, f'{dataset_name}.{version}.sentence-LL.average_byNrTokens.txt')
             print(out_name)
             with open(out_name, "w") as fout:
                 for i, sent, sent_score, nr_tok, nr_word in zip(range(len(sentences)), sentences, sent_scores_avg_by_nrtokens, nr_tokens, nr_words):
                     fout.write(f'{i}\t{sent}\t{sent_score}\t{nr_tok}\t{nr_word}\n')
-                    
+
             out_name = os.path.join(out_dir, f'{dataset_name}.{version}.sentence-LL.average_byNrWords.txt')
             print(out_name)
             with open(out_name, "w") as fout:
@@ -102,3 +119,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
